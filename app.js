1b9b40903a2fd6587786e8898401ce023022e02a
Igor Choromanski <daedalia.3d@gmail.com>
2022-09-05 14:50:40 -0600

    basic vite setup

---
+++ app.js
@@ -0,0 +1,21 @@
+(function () {
+
+  init();
+
+  function init() {
+    console.log('this is vite!');
+  }
+
+  function showFrom() {
+
+  }
+
+  function hideForm() {
+
+  }
+
+  function showDinosaurs() {
+
+  }
+
+})();
