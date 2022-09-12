613715a433651bd0c8d35db7b10962fec4c2eb9d
Igor Choromanski <daedalia.3d@gmail.com>
2022-09-12 10:26:14 -0600

    feat: Hides form, loads graphic, adds comparison functions

--- app.mjs
+++ app.mjs
@@ -1,27 +1,59 @@
-import mForm from './src/form';
 import mGraphic from './src/graphic';
 
 (function () {
 
-  init();
+  const weightBox = document.getElementById('weight');
+  const heightFeetBox = document.getElementById('height_feet');
+  const heightInchesBox = document.getElementById('height_inches');
+  const nameBox = document.getElementById('name');
+  const dietBox = document.getElementById('diet');
+  const whenBox = document.getElementById('when');
+  const factBox = document.getElementById('fact');
+  const inputForm = document.getElementById('form');
 
-  function init() {
-    let form = new mForm();
-    let formInstance = form.create();
-    let graphic = new mGraphic();
-    graphic.show();
-  }
+  document.getElementById('formButton').onclick = showDinosaurs;
 
-  function showInputForm() {
-
-  }
-
-  function hideInputForm() {
-
-  }
+  const graphic = new mGraphic();
 
   function showDinosaurs() {
+    const humanData = processInputData();
+    inputForm.remove();
+    graphic.show(humanData);
+  }
 
+  function processInputData() {
+    let humanData = {};
+    humanData.weight = getValidatedWeight();
+    humanData.height = getValidatedHeight();
+    humanData.name = nameBox.value || 'No name provided.';
+    humanData.diet = dietBox.value;
+    humanData.when = whenBox.value || 'No time period provided.';
+    humanData.fact = factBox.value || 'No fact provided';
+    console.log(humanData);
+  }
+
+  function getValidatedWeight() {
+    if (typeof (weightBox.value) == 'number' && weightBox.value > 0)
+      return weightBox.value;
+    return 0;
+  }
+
+  function getValidatedHeight() {
+    let validFeet = 0;
+    let validInches = 0;
+
+    if (typeof (heightFeetBox.value) == 'number' && heightFeetBox.value > 0)
+      validFeet = heightFeetBox.value;
+
+    if (typeof (heightInchesBox.value) == 'number' && heightInchesBox.value > 0)
+      validInches = heightInchesBox.value;
+
+    if (validFeet || validInches) {
+      const heightInInches = validFeet * 12 + validInches;
+      return heightInInches;
+    }
+
+    return 0;
   }
 
 })();
