a10626996a4e94e8280f619e2d3aee71627e7727
Igor Choromanski <daedalia.3d@gmail.com>
2022-09-20 09:46:36 -0600

    refactor: Move form to module, fix: correct height and weight validation

--- src/form.mjs
+++ src/form.mjs
@@ -1,12 +1,47 @@
-export default function () {
+export default function (graphic) {
 
-  function create() {
-    console.log('form created');
-    return 5;
+  const weightBox = document.getElementById('weight');
+  const heightFeetBox = document.getElementById('height_feet');
+  const heightInchesBox = document.getElementById('height_inches');
+  const nameBox = document.getElementById('name');
+  const dietBox = document.getElementById('diet');
+  const whenBox = document.getElementById('when');
+  const factBox = document.getElementById('fact');
+  const inputForm = document.getElementById('form');
+
+  document.getElementById('formButton').onclick = showDinosaurs;
+
+  function showDinosaurs() {
+    const humanData = processInputData();
+    inputForm.remove();
+    graphic.show(humanData);
   }
 
-  return {
-    create: create
-  };
+  function processInputData() {
+    let humanData = {};
+    humanData.weight = getValidatedWeight();
+    humanData.height = getValidatedHeight();
+    humanData.name = nameBox.value || 'No name provided.';
+    humanData.diet = dietBox.value;
+    humanData.when = whenBox.value || 'No time period provided.';
+    humanData.fact = factBox.value || 'No fact provided';
+    console.log(humanData);
+    return humanData;
+  }
+
+  function getValidatedWeight() {
+    const validWeight = Number(weightBox.value) ? Number(weightBox.value) : 0;
+    return validWeight;
+  }
+
+  function getValidatedHeight() {
+
+    const validFeet = Number(heightFeetBox.value) ? Number(heightFeetBox.value) : 0;
+    const validInches = Number(heightInchesBox.value) ? Number(heightInchesBox.value) : 0;
+
+    const heightInInches = validFeet * 12 + validInches;
+    return heightInInches;
+
+  }
 
 }
